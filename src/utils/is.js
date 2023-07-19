14e048ccb059d4ac7f9efe22ac7447b620bcf766
zijianlin <1031531798@qq.com>
2023-07-19 11:16:23 +0800

    feat(utils): 添加is 判断函数

--- src/utils/is.js
+++ src/utils/is.js
@@ -1,6 +1,3 @@
-import { enumApproveStatus } from '@/config/enum'
-import website from '@/config/website'
-import store from '@/store/index.js'
 export function is (val, type) {
   return toString.call(val) === `[object ${type}]`
 }
@@ -34,19 +31,9 @@ export function isString (val) {
   return is(val, 'String')
 }
 
-export function isShipServeRole () {
-  try {
-    if (store.getters.roles[0].roleType === website.roleType.shipServeCompany) {
-      return true
-    }
-  } catch {
-    return false
-  }
-  return false
-}
 // 密码强度判断
 export function checkPasswordLevel (rule, value, callback) {
-  var pwdRegex = new RegExp('(?=.*[0-9])(?=.*[a-zA-Z]).{8,30}')
+  let pwdRegex = new RegExp('(?=.*[0-9])(?=.*[a-zA-Z]).{8,30}')
   if (pwdRegex.test(value)) {
     callback()
   } else {
@@ -68,24 +55,4 @@ export function hasFindIndex (value, arr) {
     return flag >= 0
   }
   return false
-}
-/**
- * @description 判断当前用户是否属于传入的权限
- * @param {检验的权限} roles
- * @returns
- */
-export function isUserRoles (roles) {
-  if (Array.isArray(roles)) {
-    const flag = roles.findIndex(item => {
-      return Number(item) === Number(store.getters.roles[0].roleType)
-    })
-    return flag >= 0
-  } else {
-    return Number(roles) === Number(store.getters.roles[0].roleType)
-  }
-}
-
-// 判断船代是否显示审核意见
-export function hasShipCompnayShowReason (row) {
-  return [enumApproveStatus.pass, enumApproveStatus.reject].includes(row.applystatus)
 }
